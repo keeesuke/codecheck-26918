15f23f4c5e6d0b1cd7421511fae5af92e6a09873
keeesuke <keisuke@testnoMacBook-Pro.local>
2018-02-05 17:40:36 +0900

    changed args

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,15 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
-		}
+			if(args.length == 0){}
+			else{
+				 if(args[0].equals("World")){
+						System.out.println("Hello World!");
+					}else if(args[0].equals("織田信長")){
+						System.out.println("Hello 織田信長!");
+					}else if(args[0].equals("codecheck")){
+						System.out.println("Hellow codecheck!");
+					}
+			}
 	}
 }
